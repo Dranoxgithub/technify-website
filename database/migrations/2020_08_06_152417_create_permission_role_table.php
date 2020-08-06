e01762da3484c93333c5fe8caa07d33a9aa726ec
vagrant <vagrant@homestead>
2020-08-06 15:33:17 +0000

    create permissions and roles

---
+++ database/migrations/2020_08_06_152417_create_permission_role_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->unsignedInteger('role_id');
+            $table->foreign('role_id', 'role_id_fk_1001475')->references('id')->on('roles')->onDelete('cascade');
+            $table->unsignedInteger('permission_id');
+            $table->foreign('permission_id', 'permission_id_fk_1001475')->references('id')->on('permissions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_role');
+    }
+}

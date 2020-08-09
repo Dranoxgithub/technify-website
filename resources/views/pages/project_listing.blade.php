dba07bd57af0b20d9bf5e1b13fa7c8992e24dc70
Anni Chen <anni.chen@duke.edu>
2020-08-08 23:47:47 -0400

    Project listing done

--- resources/views/pages/project_listing.blade.php
+++ resources/views/pages/project_listing.blade.php
@@ -13,13 +13,25 @@
 <div class="row gallery">
 	
 	@foreach($projects as $project)
-	<div class="col-lg-3 col-md-4 col-sm-6 gallery-box">
-		<div class="image fit">
-			<img src="images/pic02.jpg" alt="" />
-		</div>
-		<div class="content">
-			<h5>{{ $project->name }}</h5>
-			<p>{{ $project->ngo->name }}</p>
+	<div class="col-lg-3 col-md-4 col-sm-6 gallery-box card">
+		
+		<div class="card-body">
+			<div class="card-title"><h5>{{ $project->name }}</h5></div>
+			<div class="card-content">
+				<button class="tag alt">{{ $project->ngo->cause }}</button>
+				<p>
+					{{ $project->ngo->name }}<br>
+				
+				{{ date('d M, Y', strtotime($project->start_date)) }} - {{ date('d M, Y', strtotime($project->end_date)) }}<br>
+				{{ $project->commitment . "h/week"}}<br>
+				{{ $project->goal }}<br>
+				</p> 
+			</div>
+			<div class="card-button">
+				<a href="/projects/{{$project->id}}" class="button">Details</a>
+			</div>
+			
+			
 		</div>
 	</div>
 		

dfcd495c56daa79f9846682ef9f75e7931045c36
Qi Xuan Khoo <khooqixuan@moneymatch.co>
2021-01-30 14:26:12 -0500

    join_us change text

--- resources/views/pages/join_us.blade.php
+++ resources/views/pages/join_us.blade.php
@@ -4,8 +4,10 @@
 @guest 
 <section id="banner">
 				<div class="content d-flex flex-column justify-content-center">
-					<h2>Please log in as a general user before signing up as a student/ NGO.</h2>
-					
+                    <h3>Hey there, thanks for checking us out. <br> You're just one step away from changing the world!</h3>
+                    <br>
+                    <h1 style="color:#f6755e">Join us now!</h1>
+                    <br>
 					<ul class="actions">
 						<li><a href="{{ route('login') }}" class="button special">Log in</a></li>
 						<li><a href="{{ route('register') }}" class="button special">Register</a></li>

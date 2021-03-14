8f1194bb93d8a8b85dc550e9f33e744ea3d6f394
Anni Chen <anni.chen@duke.edu>
2021-03-14 17:02:36 -0400

    Anni Chen image replaced to local version on about us

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -41,7 +41,7 @@
             
             <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
                 <div class="team text-center rounded p-3 py-4">
-                    <img src="https://media-exp1.licdn.com/dms/image/C4D03AQH1HGzAAzKyAA/profile-displayphoto-shrink_400_400/0/1591807679648?e=1615420800&v=beta&t=ZX9-rFfKefNpGCKQzVmPmEzg35ZF_1BRowhQxM4Jk70" class="img-fluid avatar avatar-medium shadow rounded-pill" alt="">
+                    <img src={{ asset('storage/images/anni_chen.jpeg') }} class="img-fluid avatar avatar-medium shadow rounded-pill" alt="">
                     <div class="content mt-3">
                         <h4 class="title mb-0">Anni Chen</h4>
                         <small class="text-muted">Co-Founder, Tech Lead</small>

9d6edf427e58c4d3f82ae2788434038aaca3b998
Rui Xin <rui.xin926@duke.edu>
2021-03-27 01:01:29 -0400

    Ensure HTTPs links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    {  
+        // force HTTPs
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }

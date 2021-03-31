986371bda443b2624ddc0103ebc15e6e0eaf329c
Rui Xin <rui.xin926@duke.edu>
2021-03-31 13:04:52 -0400

    Fix footer + welcome page

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -108,49 +108,50 @@
 
 	<div class="body-height-grower"></div>
 
-
 	<!-- Footer -->
-	<div class="row footer" style="background-color:#9ECACC">
-		<div class="col-10 d-flex p-2 flex-row flex-md-nowrap flex-wrap justify-content-between align-items-top">
-			<div class="col-sm-6 p-4 footer-col-1 d-inline-flex flex-row align-items-center mr-4">
-				<img class="p-2 m-2 img-fluid"src="/images/bulb.png" alt="">
-				<div class="bottom-text text-left" style="line-height: 2.0;"> 
-					Technify provides pro bono tech consulting and
-					innovative digital solutions to social enterprises
-					and NGOs in Southeast Asia through a network 
-					of volunteers from U.S. colleges. 
-				</div>
-			</div>
-			<div class="col-sm-4 p-2 footer-col-2 mt-4 text-center">
-				<p class="footer-header">Contact Us</p> 
-				<div>
-					<div class="mb-2">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-							<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-						</svg>
-						Duke University, NC, USA 27705
+	<footer style="background-color:#9ECACC">
+		<div class="container-fluid">
+			<div class="row d-flex p-2 flex-row flex-md-nowrap flex-wrap justify-content-between align-items-top">
+				<div class="col-md-4 p-4 footer-col-1 d-inline-flex flex-row align-items-center">
+					<img class="p-2 m-2 img-fluid"src="/images/bulb.png" alt="">
+					<div class="bottom-text text-left" style="line-height: 2.0;"> 
+						Technify provides pro bono tech consulting and
+						innovative digital solutions to social enterprises
+						and NGOs in Southeast Asia through a network 
+						of volunteers from U.S. colleges. 
 					</div>
+				</div>
+				<div class="col-md-4 p-2 footer-col-2 mt-4 text-center">
+					<p class="footer-header">Contact Us</p> 
 					<div>
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
-							<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
-						</svg>
-						technifyinitiative@gmail.com
+						<div class="mb-2">
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+								<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+							</svg>
+							Duke University, NC, USA 27705
+						</div>
+						<div>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+								<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+							</svg>
+							technifyinitiative@gmail.com
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="col-sm-2 p-2 footer-col-4 mt-4 text-center">
-				<p class="footer-header">About Us</p>
-				<a href="">Who We Are</a><br>
-				<a href="">Projects</a><br>
-				<a href="">Our Team</a><br>
-			</div>
-			<div class="col-sm-2 p-2 footer-col-5 mt-4 mb-4 text-center">
-				<p class="footer-header">Partner With Us</p>
-				<a href="">Sign Up</a><br>
-				<a href="">Log In</a>
+				<div class="col-md-2 p-2 footer-col-4 mt-4 text-center">
+					<p class="footer-header">About Us</p>
+					<a href="">Who We Are</a><br>
+					<a href="">Projects</a><br>
+					<a href="">Our Team</a><br>
+				</div>
+				<div class="col-md-2 p-2 footer-col-5 mt-4 mb-4 text-center">
+					<p class="footer-header">Partner With Us</p>
+					<a href="">Sign Up</a><br>
+					<a href="">Log In</a>
+				</div>
 			</div>
 		</div>
-	</div>
+	</footer>
 
 
 	<!-- Script -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
 
-<section class="row col-12 justify-content-center">
-    <div class="d-flex flex-row m-4 p-2  justify-content-center">
-        <div class="col-8 p-2 text-center">
-            <h1>Transform and empower with tech</h1>
-            <p class="p-2" style="line-height: 1.6">
-                We provide pro bono tech consulting and innovative digital solutions by connecting 
-                top tech talents at U.S. colleges with social enterprises and NGOs in Southeast Asia
-            </p>
+<section>
+    <div class="container">
+        <div class="row m-4 p-2  justify-content-center">
+            <div class="col-10 p-2 text-center">
+                <h1>Transform and empower with tech</h1>
+                <p class="p-2" style="line-height: 1.6">
+                    We provide pro bono tech consulting and innovative digital solutions by connecting 
+                    top tech talents at U.S. colleges with social enterprises and NGOs in Southeast Asia
+                </p>
+            </div>
         </div>
     </div>
 </section>
@@ -18,52 +20,54 @@
 
 {{-- Workflow --}}
 
-<section class="row col-12 justify-content-center">
-    <div class="text-center m-4">
-        <span style="height:97px;">
+<section>
+    <div class="container">
+        <div class="row text-center m-4">
             <h2 class="p-2 m-2">
                 We help Southeast Asian changemakers overcome obstacles
                 to sustainable change. <br><br> Here’s how it works.
             </h2>
-        </span>
-    </div>
-    <div class="d-flex flex-row flex-md-nowrap flex-wrap justify-content-center">
-        <div class="p-2 text-center">
-            <img class="p-2" src="/images/ellipse_blue.png" alt="">
-            <h5 class="p-3">Create Your Profile</h5>
-            <p>
-                Sign up to volunteer as a student 
-                developer or apply as a non-profit 
-                organization to submit new projects
-            </p>
         </div>
-        <div class="p-2 text-center">
-            <img class="p-2" src="/images/ellipse_yellow.png" alt="">
-            <h5 class="p-3">Find Your Match</h5>
-            <p>
-                Our team will match student developers
-                with NGOs/social enterprises based on 
-                their interests and values.
-            </p>
-        </div>
-        <div class="p-2 text-center mb-5">
-            <img class="p-2" src="/images/ellipse_green.png" alt="">
-            <h5 class="p-3">Serve and Contribute</h5>
-            <p>
-                Students will consult with their partner 
-                organization to develop digital solutions
-                based on their project specifications
-            </p>
+        <div class="row mb-5">
+            <div class="col-md-4 p-2 text-center">
+                <img class="p-2" src="/images/ellipse_blue.png" alt="">
+                <h5 class="p-3">Create Your Profile</h5>
+                <p>
+                    Sign up to volunteer as a student 
+                    developer or apply as a non-profit 
+                    organization to submit new projects
+                </p>
+            </div>
+            <div class="col-md-4 p-2 text-center">
+                <img class="p-2" src="/images/ellipse_yellow.png" alt="">
+                <h5 class="p-3">Find Your Match</h5>
+                <p>
+                    Our team will match student developers
+                    with NGOs/social enterprises based on 
+                    their interests and values.
+                </p>
+            </div>
+            <div class="col-md-4 p-2 text-center">
+                <img class="p-2" src="/images/ellipse_green.png" alt="">
+                <h5 class="p-3">Serve and Contribute</h5>
+                <p>
+                    Students will consult with their partner 
+                    organization to develop digital solutions
+                    based on their project specifications
+                </p>
+            </div>
         </div>
     </div>
 </section>
 
 {{-- Featured Projects --}}
 
-<section id="Projects" class="container-fluid " style="background-color: #F4F4F4">
-    <div class="row col-12 align-middle">
-         <h2 class="m-4">Featured Projects</h2>
-        <div class="d-flex flex-row flex-md-nowrap flex-wrap justify-content-around p-2">
+<section id="Projects" style="background-color: #F4F4F4">
+    <div class="container">
+        <div class="row">
+            <h2 class="m-4">Featured Projects</h2>
+        </div>
+        <div class="row justify-content-around p-2">
             {{-- add blade logic for all featured projects --}}
             <div class="card shadow p-3 mb-5 bg-white rounded m-2 col-md-5">
                 <img class="card-img-top img-fluid" src="/images/qwe.png" alt="" style="width:100%; height:15vw; object-fit:cover;">
@@ -95,41 +99,50 @@
 
 {{-- Sponsors --}}
 
-<section class="row col-12 align-middle justify-content-center">
-    <div class="col-8 p-2 mb-4 text-center">We are proudly sponsored by</div>
-    <div class="d-flex col-12 flex-row flex-wrap flex-md-nowrap justify-content-around align-items-center">
-        <div class="p-2 mb-2">
-            <img src="/images/US.png" alt="">
+<section>
+    <div class="container">
+        <div class="row justify-content-center my-4">
+            <h4>We are proudly sponsored by</h4>
         </div>
-        <div class="p-2 mb-2">
-            <img src="/images/yseali.png" alt="">
+        <div class="row justify-content-around align-items-center">
+            <div class="p-2 mb-2">
+                <img src="/images/US.png" alt="">
+            </div>
+            <div class="p-2 mb-2">
+                <img src="/images/yseali.png" alt="">
+            </div>
+            <div class="p-2 mb-2">
+                <img src="/images/culturalvistas.png" alt="">
+            </div>
+            <div class="p-2">
+                <img src="/images/colab.png" alt="">
+            </div>
         </div>
-        <div class="p-2 mb-2">
-            <img src="/images/culturalvistas.png" alt="">
-        </div>
-        <div class="p-2">
-            <img src="/images/colab.png" alt="">
-        </div>
-    </div>
-    <div class="p-2 m-2 overflow-hidden">
-        <img src="/images/hor_vector.png" alt="">
+        <hr class="my-4"/>
+        <!-- <div class="p-2 m-2 overflow-hidden">
+            <img src="/images/hor_vector.png" alt="">
+        </div> -->
     </div>
 </section>
 
 {{-- Join Us --}}
-<section class="row col-12 align-middle justify-content-center text-center">
-    <h2><span class="p-2">We believe in tech, but we believe more in the humanity behind tech.</span></h2>
-    <p class="p-2">
-        Join us now to find out how you can help bridge the digital gap
-        in Southeast Asia and change tens of thousands of lives!
-    </p>
-    <div class="d-flex col-md-8 flex-row flex-wrap flex-md-nowrap justify-content-around align-items-center mb-4">
-        <!-- <a class="btn btn-lg btn-primary mb-2" href="" style="background-color: #26484A">
-            Apply as Non-Profit
-        </a> -->
-        <a class="btn btn-lg btn-primary" href="/register?type=student" style="background-color: #26484A">
-            Apply as Volunteer
-        </a>
+<section>
+    <div class="container mb-4">
+        <div class="row text-center">
+            <h2><span class="p-2">We believe in tech, but we believe more in the humanity behind tech.</span></h2>
+            <p class="p-2">
+                Join us now to find out how you can help bridge the digital gap
+                in Southeast Asia and change tens of thousands of lives!
+            </p>
+        </div>
+        <div class="row justify-content-around align-items-center">
+            <!-- <a class="btn btn-lg btn-primary" href="" style="background-color: #26484A">
+                Apply as Non-Profit
+            </a> -->
+            <a class="btn btn-lg btn-primary" href="/register?type=student" style="background-color: #26484A">
+                Apply as Volunteer
+            </a>
+        </div>
     </div>
 </section>
 

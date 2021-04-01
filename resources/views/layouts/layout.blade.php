d2a7b714149393544b14232824c39dc00f32724f
Rui Xin <rui.xin926@duke.edu>
2021-03-31 20:49:37 -0400

    Merge master + fix display bugs

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,18 +8,18 @@
 	@else
 		<title>Technify</title>
 	@endif
-	<link rel="stylesheet" href="/css/layout.css" />
+	<link rel="stylesheet" href="/assets/css/layout.css" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 	<!-- Navigation -->
 	<nav class="navbar navbar-expand-lg navbar-custom sticky-top navbar-light">
-		<div class="container col-11 p-2 mx-auto">
+		<div class="container-fluid col-11 mx-auto">
 			<a class="navbar-brand" href="/">
 				<img id="navbar-logo" class="img-fluid" src="/images/logo.png" alt="">
 			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="true" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon navbar-light"></span>
+			<button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="true" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
 			</button>
 
 			<div class="collapse navbar-collapse float-right" id="navbarResponsive">
@@ -33,8 +33,8 @@
 					<li class="nav-item">
 						<a class="nav-link" href="/about_us">Our Team</a>
 					</li>
-					<li class="nav-item d-none d-lg-block mx-3">
-						<a class="navbar-text hor-vector"> | </a>
+					<li class="nav-item d-none d-lg-block">
+						<a class="nav-link hor-vector"> | </a>
 					</li>
 					<hr class="w-100 d-lg-none">
 					@guest
@@ -45,14 +45,14 @@
 							</li>                        
 						@elseif (Request::is('login'))
 							<li class="nav-item">
-								<a class="nav-link btn btn-outline-light" href="/register?type=student">Sign Up</a>
+								<a class="nav-link btn btn-light" href="/register?type=student">Sign Up</a>
 							</li>                        
 						@else
 							<li class="nav-item">
 								<a class="nav-link" href="/login">Log in</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link btn btn-outline-light" href="/register?type=student">Sign Up</a>
+								<a class="nav-link btn btn-light" href="/register?type=student">Sign Up</a>
 							</li>
 						@endif
 								
@@ -111,8 +111,8 @@
 	<!-- Footer -->
 	<footer style="background-color:#9ECACC">
 		<div class="container-fluid">
-			<div class="row d-flex p-2 flex-row flex-md-nowrap flex-wrap justify-content-between align-items-top">
-				<div class="col-md-4 p-4 footer-col-1 d-inline-flex flex-row align-items-center">
+			<div class="row flex-row flex-md-nowrap flex-wrap justify-content-between align-items-top my-5 mx-4">
+				<div class="col-md-4 d-none d-md-inline-flex footer-col-1 flex-row align-items-center">
 					<img class="p-2 m-2 img-fluid"src="/images/bulb.png" alt="">
 					<div class="bottom-text text-left" style="line-height: 2.0;"> 
 						Technify provides pro bono tech consulting and
@@ -121,7 +121,7 @@
 						of volunteers from U.S. colleges. 
 					</div>
 				</div>
-				<div class="col-md-4 p-2 footer-col-2 mt-4 text-center">
+				<div class="col-md-3 p-2 footer-col-2 mt-4">
 					<p class="footer-header">Contact Us</p> 
 					<div>
 						<div class="mb-2">
@@ -138,13 +138,16 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-md-2 p-2 footer-col-4 mt-4 text-center">
+				<div class="d-none d-md-block mt-4 footer-col-3 ">
+					<img class="image-fluid mx-auto" src="/images/vertical_vector.png" alt="">
+				</div>
+				<div class="col-md-2 d-none d-md-block p-2 footer-col-4 mt-4">
 					<p class="footer-header">About Us</p>
 					<!-- <a href="">Who We Are</a><br> -->
 					<a href="/#Projects">Projects</a><br>
 					<a href="/about_us">Our Team</a><br>
 				</div>
-				<div class="col-md-2 p-2 footer-col-5 mt-4 mb-4 text-center">
+				<div class="col-md-2 d-none d-md-block p-2 footer-col-5 mt-4 mb-4">
 					<p class="footer-header">Partner With Us</p>
 					<a href="/register?type=student">Sign Up</a><br>
 					<a href="/login">Log In</a>
@@ -152,6 +155,53 @@
 			</div>
 		</div>
 	</footer>
+    <!-- <section class="container-fluid" style="">
+        <div class="row footer" style="background-color:#9ECACC;">
+            <div class="col-10 d-flex pl-sm-5 p-2 pb-5 pt-4 flex-row flex-md-nowrap flex-wrap justify-content-between align-items-top">
+                <div class="col-sm-4 p-2 footer-col-1 flex-row align-items-center mr-5 ml-5">
+                    <img class="p-2 m-2 img-fluid footer-img"src="/images/bulb.png" alt="">
+                    <div class="bottom-text text-left"> 
+                        Technify provides pro bono tech consulting and
+                        innovative digital solutions to social enterprises
+                        and NGOs in Southeast Asia through a network 
+                        of volunteers from U.S. colleges. 
+                    </div>
+                </div>
+                <div class="col-sm-3 p-2 footer-col-2 mt-4 ml-sm-3 text-left">
+                    <p class="footer-header">Contact Us</p> 
+                    <div>
+                        <div class="mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                            </svg>
+                            Duke University, NC, USA 27705
+                        </div>
+                        <div class="d-flex d-inline-flex align-items-center justify-content-around">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+                            </svg>
+                            <div class="ml-1">technifyinitiative@gmail.com</div> 
+                        </div>
+                    </div>
+                </div>
+				<div class="p-2 pr-5 mt-4 footer-col-3 mx-5">
+					<img class="image-fluid" src="/images/vertical_vector.png" alt="">
+				</div>
+                <div class="col-sm-2 p-2 pl-4 footer-col-4 mt-4 ml-5 text-left">
+                    <p class="footer-header">About Us</p>
+                    <a class="" href="">Who We Are</a><br>
+                    <a class="" href="">Projects</a><br>
+                    <a class="" href="">Our Team</a><br>
+                </div>
+                <div class="col-sm-2 p-2 footer-col-5 mt-4 mb-4 text-left">
+                    <p class="footer-header">Partner With Us</p>
+                    <a href="">Sign Up</a><br>
+                    <a href="">Log In</a>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
 
 
 	<!-- Script -->

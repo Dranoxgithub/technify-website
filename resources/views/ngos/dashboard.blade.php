97ec9bcc503c7f8a6b94ab432cbdbacece837e7d
Rui Xin <rui.xin926@duke.edu>
2021-06-13 23:28:49 -0400

    Finish three buttons

--- resources/views/ngos/dashboard.blade.php
+++ resources/views/ngos/dashboard.blade.php
@@ -20,30 +20,30 @@
 						<div class="form-group row">
 							<label class="col-6 col-form-label">{{ __('Contact Email') }}</label>
 							<label class="col-6 col-form-label">{{ Auth::user()->email }}</label>
-						</div>						
+						</div>
 
 						<div class="form-group row">
 							<label class="col-6 col-form-label">{{ __('NGO Name') }}</label>
 							<label class="col-6 col-form-label">{{ $ngo->name }}</label>
 						</div>
-						
+
 						<div class="form-group row">
 							<label class="col-6 col-form-label">{{ __('NGO Website') }}</label>
 							<label class="col-6 col-form-label">{{ $ngo->website }}</label>
 						</div>
-						
+
 						<div class="form-group row">
 							<label class="col-6 col-form-label">{{ __('Causes') }}</label>
 							<label class="col-6 col-form-label">{{ $ngo->cause }}</label>
 						</div>
 
-                        <div class="form-group row mb-0 justify-content-center pt-4">
-                            <div>
-                                <a href="/logout" class="btn btn-primary w-100">
-                                    {{ __('Logout') }}
-                                </a>
-                            </div>
-                        </div>
+						<div class="form-group row mb-0 justify-content-center pt-4">
+							<div>
+								<a href="/logout" class="btn btn-primary w-100">
+									{{ __('Logout') }}
+								</a>
+							</div>
+						</div>
 					</form>
 				</div>
 			</div>
@@ -52,65 +52,24 @@
 		<!-- Project display -->
 		<div class="col-md-8 my-3">
 			<div class="card">
-				<div class="card-body p-5">
-					<h4 class="card-title font-weight-bold pb-3">Login</h4>
-					<form method="POST" action="{{ route('login') }}">
-						@csrf
+				<div class="card-body p-4 display-flex justify-content-between">
+					<div class="card-title pb-3" style="margin-top:-.3125rem">
+							<h4 class="navbar-brand font-weight-bold mr-5" style="width:auto;">Projects</h4>
+							<form class="form-inline justify-content-between">
+								<input class="form-control mr-sm-2 search" type="search" placeholder="Search..." aria-label="Search">
+								<a href="#" class="btn btn-primary">
+										{{ __('Add Project') }}
+								</a>
+							</form>
+						<!-- <h4>
+							<span class="font-weight-bold">Login</span>
+							<form class="form-inline">
+								<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+							</form>
+						</h4> -->
 
-						<div class="form-group row">
-							<label for="email" class="col-form-label pt-4">{{ __('E-Mail Address') }}</label>
+					</div>
 
-							<div>
-								<input id="email" type="email" class="form-control @error('email') is-invalid @enderror px-0" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-								@error('email')
-								<span class="invalid-feedback" role="alert">
-									<strong>{{ $message }}</strong>
-								</span>
-								@enderror
-							</div>
-						</div>
-
-						<div class="form-group row">
-							<label for="password" class="col-form-label pt-4">{{ __('Password') }}</label>
-
-							<div>
-								<input id="password" type="password" class="form-control @error('password') is-invalid @enderror px-0" name="password" required autocomplete="current-password">
-
-								@error('password')
-								<span class="invalid-feedback" role="alert">
-									<strong>{{ $message }}</strong>
-								</span>
-								@enderror
-							</div>
-						</div>
-
-						<div class="form-group row">
-							<div>
-								<div class="form-check">
-									<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-									<label class="form-check-label" for="remember">
-										{{ __('Remember Me') }}
-									</label>
-								</div>
-							</div>
-						</div>
-
-
-						<div class="form-group row mb-0 justify-content-center pt-4">
-							<div>
-								<button type="submit" class="btn btn-primary w-100">
-									{{ __('Login') }}
-								</button>
-							</div>
-							@if (Route::has('password.request'))
-							<a class="btn btn-link" href="{{ route('password.request') }}">
-								{{ __('Forgot Your Password?') }}
-							</a>
-							@endif
-						</div>
-					</form>
 				</div>
 			</div>
 		</div>

e967eb671d10507fb624a6ede56f80564b9174f0
Rui Xin <rx31@duke.edu>
2021-10-01 20:11:11 -0400

    Add add_project_verified_column migration

---
+++ database/migrations/2021_10_02_000739_add_project_verified_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProjectVerifiedColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('projects', function (Blueprint $table) {
+            $table->boolean('verified')->default(false);
+        });       //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table('projects', function (Blueprint $table) {
+            //
+            $table->dropColumn('verified');
+        });       //
+    }
+}

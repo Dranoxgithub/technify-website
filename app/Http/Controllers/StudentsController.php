43143e32cbc4c0a7eb3a368e69ec09b8f52a3075
Anni Chen <anni.chen@duke.edu>
2022-01-28 15:55:48 -0500

    Update student outreach gmail

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 use App\Student; 
-use App\Mail\StudentAppEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;

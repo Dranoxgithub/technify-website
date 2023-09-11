59d84cfc929168bcf2d5de734c7d4b6e66a2cfae
Jason Yang <canyoncrestjy@gmail.com>
2023-09-11 15:43:13 -0400

    updated more

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -34,12 +34,12 @@
 
         <div class="col-lg-3 col-md-6 col-12 pt-2">
             <div class="team text-center rounded p-3 py-4">
-                <img src="images/Lily.jpg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
+                <img src="images/HannahLawry.jpg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
                 <div class="content mt-3">
-                    <h4 class="title mb-0"><a href="https://www.linkedin.com/in/lily-li-58a9821a2/" target="blank" style="color:#26484A">Lily Li</a></h4>
-                    <small class="text-muted">Partner Outreach Lead</small>
-                    <p>Undergraduate student at Duke University studying Statistics and Psychology. Passionate about data-driven impactful work that connects and empowers. 
-                        An enthusiast of musicals, nature, and FRIENDS! </p>
+                    <h4 class="title mb-0"><a href="https://www.linkedin.com/in/hannah-lawry-379782219/" target="blank" style="color:#26484A">Hannah Lawry</a></h4>
+                    <small class="text-muted">Director of Product</small>
+                    <p>Undergraduate student at Duke studying Computer Science, with a minor in Economics, and a certificate in Decision Sciences. 
+                        Excited about the impact of tech and data on every industry. Loves to run and travel.</p>
                     <ul class="list-unstyled mt-3 social-icon social mb-0">
                         <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
                         <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>

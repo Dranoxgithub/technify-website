ec5c083b84f2a077961446720c036217f5155c21
Jason Yang <canyoncrestjy@gmail.com>
2023-09-11 13:42:01 -0400

    row edit

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -6,7 +6,7 @@
 <section class="container-fluid my-4">
     <div id="banner" class="row p-4 justify-content-center">
         <div class="content d-flex flex-column justify-content-center text-center">
-            <h1>Meet our team</h1>
+            <h1>Meet our Executive Team</h1>
             <h5>We are a group of students believing in tech for social good.</h5>
         </div>
     </div>
@@ -111,102 +111,104 @@
 <section class="container-fluid my-4">
     <div id="banner" class="row p-4 justify-content-center">
         <div class="content d-flex flex-column justify-content-center text-center">
-            <h1>Our Founder and Alumus</h1>
+            <h1>Our Founder and Alumni</h1>
             <h5>Meet the people who started it all.</h5>
         </div>
     </div>
 </section>
 
 <div class="container bootdey">
-    <div class="col-lg-3 col-md-6 col-12  pt-2">
-        <div class="team text-center rounded p-3 py-4">
-            <img src="images/Khoo.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
-            <div class="content mt-3">
-                <h4 class="title mb-0"><a href="https://www.linkedin.com/in/qixuan-khoo/" target="blank" style="color:#26484A">Qi Xuan Khoo</a></h4>
-                <small class="text-muted">Alumnus (Co-Founder, Co-Director)</small>
-                <p>Karsh Scholar. A part-time musician, full-time foodie, and a dreamer who aspires to inspire before he expires.</p>
-                <ul class="list-unstyled mt-3 social-icon social mb-0">
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
-                </ul><!--end icon-->
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-12  pt-2">
+            <div class="team text-center rounded p-3 py-4">
+                <img src="images/Khoo.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
+                <div class="content mt-3">
+                    <h4 class="title mb-0"><a href="https://www.linkedin.com/in/qixuan-khoo/" target="blank" style="color:#26484A">Qi Xuan Khoo</a></h4>
+                    <small class="text-muted">Alumnus (Co-Founder, Co-Director)</small>
+                    <p>Karsh Scholar. A part-time musician, full-time foodie, and a dreamer who aspires to inspire before he expires.</p>
+                    <ul class="list-unstyled mt-3 social-icon social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
             </div>
         </div>
+
+        <div class="col-lg-3 col-md-6 col-12  pt-2">
+            <div class="team text-center rounded p-3 py-4">
+                <img src="images/Shine.jpg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
+                <div class="content mt-3">
+                    <h4 class="title mb-0"><a href="https://www.linkedin.com/in/shinezwu/" target="blank" style="color:#26484A">Shine Wu</a></h4>
+                    <small class="text-muted">Alumnus (Co-Founder, Co-Director)</small>
+                    <p>Robertson Scholar. Lover of the outdoors, climate and frontier tech investor, and avid basketball fan.</p>
+                    <ul class="list-unstyled mt-3 social-icon social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-12 pt-2">
+            <div class="team text-center rounded p-3 py-4">
+                <img src="images/Anni.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
+                <div class="content mt-3">
+                    <h4 class="title mb-0"><a href="https://www.linkedin.com/in/anni-chen-02/" target="blank" style="color:#26484A">Anni Chen</a></h4>
+                    <small class="text-muted">Alumnus (Co-founder)</small>
+                    <p>Passionate about women in tech and tech for social good.</p>
+                    <ul class="list-unstyled mt-3 social-icon social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
+            </div>
+        </div><!--end col-->
+
+        <div class="col-lg-3 col-md-6 col-12 pt-2">
+            <div class="team text-center rounded p-3 py-4">
+                <img src="images/Raj.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
+                <div class="content mt-3">
+                    <h4 class="title mb-0"><a href="https://www.linkedin.com/in/tharunraj7/" target="blank" style="color:#26484A">Tharun Raj</a></h4>
+                    <small class="text-muted">Alumnus (Product Management)</small>
+                    <p>A fan of all things tech, with a sprinkle of philosophy into the mix.</p>
+                    <ul class="list-unstyled mt-3 social-icon social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
+            </div>
+        </div><!--end col-->
+
+        <div class="col-lg-3 col-md-6 col-12 pt-2">
+            <div class="team text-center rounded p-3 py-4">
+                <img src="images/Rui.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
+                <div class="content mt-3">
+                    <h4 class="title mb-0"><a href="https://www.linkedin.com/in/rui-xin-8070181b9/" target="blank" style="color:#26484A">Rui Xin</a></h4>
+                    <small class="text-muted">Alumnus (Tech Lead)</small>
+                    <p>Interested in how everything works.</p>
+                    <ul class="list-unstyled mt-3 social-icon social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
+            </div>
+        </div><!--end col-->
     </div>
-
-    <div class="col-lg-3 col-md-6 col-12  pt-2">
-        <div class="team text-center rounded p-3 py-4">
-            <img src="images/Shine.jpg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
-            <div class="content mt-3">
-                <h4 class="title mb-0"><a href="https://www.linkedin.com/in/shinezwu/" target="blank" style="color:#26484A">Shine Wu</a></h4>
-                <small class="text-muted">Alumnus (Co-Founder, Co-Director)</small>
-                <p>Robertson Scholar. Lover of the outdoors, climate and frontier tech investor, and avid basketball fan.</p>
-                <ul class="list-unstyled mt-3 social-icon social mb-0">
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
-                </ul><!--end icon-->
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-3 col-md-6 col-12 pt-2">
-        <div class="team text-center rounded p-3 py-4">
-            <img src="images/Anni.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
-            <div class="content mt-3">
-                <h4 class="title mb-0"><a href="https://www.linkedin.com/in/anni-chen-02/" target="blank" style="color:#26484A">Anni Chen</a></h4>
-                <small class="text-muted">Alumnus (Co-founder)</small>
-                <p>Passionate about women in tech and tech for social good.</p>
-                <ul class="list-unstyled mt-3 social-icon social mb-0">
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
-                </ul><!--end icon-->
-            </div>
-        </div>
-    </div><!--end col-->
-
-    <div class="col-lg-3 col-md-6 col-12 pt-2">
-        <div class="team text-center rounded p-3 py-4">
-            <img src="images/Raj.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
-            <div class="content mt-3">
-                <h4 class="title mb-0"><a href="https://www.linkedin.com/in/tharunraj7/" target="blank" style="color:#26484A">Tharun Raj</a></h4>
-                <small class="text-muted">Alumnus (Product Management)</small>
-                <p>A fan of all things tech, with a sprinkle of philosophy into the mix.</p>
-                <ul class="list-unstyled mt-3 social-icon social mb-0">
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
-                </ul><!--end icon-->
-            </div>
-        </div>
-    </div><!--end col-->
-
-    <div class="col-lg-3 col-md-6 col-12 pt-2">
-        <div class="team text-center rounded p-3 py-4">
-            <img src="images/Rui.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill profile-pic" alt="">
-            <div class="content mt-3">
-                <h4 class="title mb-0"><a href="https://www.linkedin.com/in/rui-xin-8070181b9/" target="blank" style="color:#26484A">Rui Xin</a></h4>
-                <small class="text-muted">Alumnus (Tech Lead)</small>
-                <p>Interested in how everything works.</p>
-                <ul class="list-unstyled mt-3 social-icon social mb-0">
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
-                    <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
-                </ul><!--end icon-->
-            </div>
-        </div>
-    </div><!--end col-->
 </div>
 
 
